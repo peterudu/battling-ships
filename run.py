5cb0da0be13598e8d400ec49ada5265bb9a537d5
peterudu <puduevborho7@gmail.com>
2023-01-02 22:53:15 +0000

    Add game boards to display battleships and shot attempts

--- run.py
+++ run.py
@@ -1,3 +1,18 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+
+game_score = {"player": 0, "computer": 0}
+
+# Board which displays the location of the battleships 
+HIDDEN_BOARD = [[" "] * 5 for x in range(5)]
+# Board which displays player's shot attempts
+GUESS_BOARD = [[" "] * 5 for i in range(5)]
+
+def print_board(board):
+    print("  A B C D E")
+    print("  +-+-+-+-+")
+    row_number = 1
+    for row in board:
+        print("%d|%s|" % (row_number, "|".join(row)))
+        row_number += 1
+
+        
