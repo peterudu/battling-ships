e18064abd768a0c34abda2c299ccce8fa1ee8354
peterudu <puduevborho7@gmail.com>
2023-01-08 11:57:20 +0000

    edit previous code and add class Gameboard for OPP

--- run.py
+++ run.py
@@ -1,18 +1,6 @@
-from random import randint
+import random
 
-game_score = {"player": 0, "computer": 0}
 
-# Board which displays the location of the battleships 
-HIDDEN_BOARD = [[" "] * 5 for x in range(5)]
-# Board which displays player's shot attempts
-GUESS_BOARD = [[" "] * 5 for i in range(5)]
-
-def print_board(board):
-    print("  A B C D E")
-    print("  +-+-+-+-+")
-    row_number = 1
-    for row in board:
-        print("%d|%s|" % (row_number, "|".join(row)))
-        row_number += 1
-
-        
+class GameBoard:
+    def __init__(self, board):
+        self.board = board
