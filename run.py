efb5851e66829196b47b74b95aa64cb306ea8c73
peterudu <puduevborho7@gmail.com>
2023-01-08 23:01:04 +0000

    Add class Battleship and create_battleship, get_player_input and count_eliminated_ships methods

--- run.py
+++ run.py
@@ -16,3 +16,44 @@ class GameBoard:
         for row in self.board:
             print("%d|%s|" % (row_number, "|".join(row)))
             row_number += 1
+
+
+# Create 3 battleships
+class Battleship:
+    def __init__(self, board):
+        self.board = board
+
+    def create_battleship(self):
+        for i in range(3):
+            self.h_row, self.v_column = randint(0, 4), randint(0, 4)
+            while self.board[self.h_row][self.v_column] == "X":
+                self.h_row, self.v_column = randint(0, 4), randint(0, 4)
+            self.board[self.h_row][self.v_column] = "X"
+        return self.board
+
+    # Get player's input
+    def get_player_input(self):
+        try:
+            h_row = input("Enter row position 1-5 for a battleship: ")
+            while h_row not in "12345":
+                print('Invalid choice, please select a valid row')
+                h_row = input("Enter row position 1-5 for a battleship: ")
+
+            v_column = input("Enter column postion A-E for a battleship: ").upper()
+            while v_column not in "ABCDE":
+                print('Invalid choice, please select a valid column')
+                v_column = input("Enter column postion A-E for a battleship: ").upper()
+                
+            return int(h_row) - 1, GameBoard.convert_letters_to_numbers()[v_column]
+        except ValueError and KeyError:
+            print("Your input is not valid")
+            return self.get_player_input()
+
+    # Number of  battleships shot successfully
+    def count_eliminated_ships(self):
+        eliminated_ships = 0
+        for row in self.board:
+            for column in row:
+                if column == "X":
+                    eliminated_ships += 1
+        return eliminated_ships
