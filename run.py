708e28c41b457ebc5976cae82e4e1dae6d3a3431
peterudu <puduevborho7@gmail.com>
2023-01-09 11:59:47 +0000

    Add RunGame function and print players game status

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ class GameBoard:
     def __init__(self, board):
         self.board = board
 
-
+    # Convert letters to numbers
     def convert_letters_to_numbers():
         letters_to_numbers = {"A": 0, "B": 1, "C": 2, "D": 3, "E": 4}
         return letters_to_numbers
@@ -18,11 +18,12 @@ class GameBoard:
             row_number += 1
 
 
-# Create 3 battleships
+
 class Battleship:
     def __init__(self, board):
         self.board = board
 
+    # Create 3 hidden battleships in computer board
     def create_battleship(self):
         for i in range(3):
             self.h_row, self.v_column = randint(0, 4), randint(0, 4)
@@ -31,7 +32,7 @@ class Battleship:
             self.board[self.h_row][self.v_column] = "X"
         return self.board
 
-    # Get player's input
+    # Get player's guess input
     def get_player_input(self):
         try:
             h_row = input("Enter row position 1-5 for a battleship: ")
@@ -57,3 +58,44 @@ class Battleship:
                 if column == "X":
                     eliminated_ships += 1
         return eliminated_ships
+
+
+def RunGame():
+    """
+    Starts a new game. Displays the game board and prompts the user
+    to guess the cordinates for the position of target battleship
+    """
+    computer_board = GameBoard([[" "] * 5 for i in range(5)])
+    players_board = GameBoard([[" "] * 5 for i in range(5)])
+    Battleship.create_battleship(computer_board)
+    # Start 5 turns
+    turns = 10
+    while turns > 0:
+        GameBoard.print_board(players_board)
+        # get player input
+        player_h_row, player_v_column = Battleship.get_player_input(object)
+        # check if guess already exist
+        while players_board.board[player_h_row][player_v_column] == "-" or players_board.board[player_h_row][player_v_column] == "X":
+            print("You have already guessed that ship position") 
+            player_h_row, player_v_column = Battleship.get_player_input(object)
+        # check for hit or miss
+        if computer_board.board[player_h_row][player_v_column] == "X":
+            print("Hurrah you successfully sunk a battleship!")
+            players_board.board[player_h_row][player_v_column] = "X"
+        else:
+            print("You missed your target shot")
+            players_board.board[player_h_row][player_v_column] = "-"
+        # check for win or lose
+        if Battleship.count_eliminated_ships(players_board) == 3:
+            print("You successfully eliminated all enemy ships!")
+            break
+        else:
+            turns -= 1
+            print(f"You have {turns} turns remaining")
+            if turns == 0:
+                print("You have no turns left")
+                GameBoard.print_board(players_board)
+                break
+
+if __name__ == '__main__':
+    RunGame()
